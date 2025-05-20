d6751cf1e89910f89a356b187a2c19b0cb127c11
bbngbnn <jethro.lagmay@ciit.edu.ph>
2025-05-21 00:39:40 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,7 +1,8 @@
 <?php
 
-/* odd or even
-function oddEven() {
+/*
+function oddEven()
+{
     $num = readline("Enter a number: ");
     $answer = $num % 2 == 0 ? "Even" : "Odd";
 
@@ -9,57 +10,59 @@ function oddEven() {
 }
 
 
-function isPrime($number) {
+function isPrime($number)
+{
     if ($number < 2) {
-            return $number. " is not prime number.\n";
-        } 
+        return $number . " is not prime number.\n";
+    }
 
-        if ($number == 2) {
-            return $number . " is a prime number.\n";
+    if ($number == 2) {
+        return $number . " is a prime number.\n";
+    }
+
+    if ($number % 2 == 0) {
+        return $number . " is not prime number.\n";
+    }
+
+    $sqrtNumber = sqrt($number);
+    for ($i = 3; $i <= $sqrtNumber; $i += 2) {
+        if ($number % $i == 0) {
+            return "$number is not a prime number\n";
         }
+    }
 
-        if ($number % 2 == 0) {
-            return $number . " is not prime number.\n";
-        }
-
-        $sqrtNumber = sqrt($number);
-        for ($i = 3; $i <= $sqrtNumber; $i+=2) {
-            if ($number % $i == 0) {
-                return "$number is not a prime number\n";
-            }
-        }
-
-        return "$number is a prime number. \n";
+    return "$number is a prime number. \n";
 }
 
 // prime number
-    $number = readline("Enter a number: ");
+$number = readline("Enter a number: ");
 
-    if(!is_numeric($number)) {
-        echo "Invalid input";
-        exit;
-    }
+if (!is_numeric($number)) {
+    echo "Invalid input";
+    exit;
+}
 
-    $number = (int)$number;
-    
-    for($ctr = 0; $ctr <= $number; $ctr++) {
-        echo isPrime($ctr);
-    }
-        */
+$number = (int)$number;
+
+for ($ctr = 0; $ctr <= $number; $ctr++) {
+    echo isPrime($ctr);
+}
+
+*/
 
 
-
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . 'vendor/autoload.php';
 use Cocur\Slugify\Slugify;
 use Ramsey\Uuid\Uuid;
-
+use demi\timelog\TimeLog;
 
 $slugify = new Slugify();
-echo $slugify->slugify("This a new title") . "\n";
+echo $slugify.slugify("This is the new sample text");
 
-$uuid = Uuid::uuid4();
-echo $uuid->toString() . "\n";
+$uuid = Uuid->uuid4();
+echo $uuid.toString();
 
-$faker = Faker\Factory::create();
-echo $faker->name() . "\n";
-echo $faker->email();
+$faker = Faker\Factory->create();
+echo $faker.name();
+echo $faker.email();
+echo $faker.text();
